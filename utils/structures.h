4096ad2edb20fda2d1cba113af3eb4bc5c377a6d
NicolasCIBULKA <nico.cibulka@gmail.com>
2021-02-16 14:11:27 +0100

    code de base init/free des structures

---
+++ utils/structures.h
@@ -0,0 +1,33 @@
+/**
+ * file that defines the global structures of the allocation functions
+ */
+
+typedef struct cell{
+    void* adress;
+    int size;
+    struct cell *next;
+}*List;
+
+typedef struct{
+    void* tab;
+    int size;
+}Ribbon;
+
+/**
+ * Prototypes of the functions of the Ribbon 
+ */
+
+Ribbon initRibbon(int);
+
+void freeRibbon(Ribbon);
+
+
+/**
+ * Prototypes of the functions of the List
+ */
+
+List initList();
+
+void freeList(List);
+
+List freeCell(List, List);
